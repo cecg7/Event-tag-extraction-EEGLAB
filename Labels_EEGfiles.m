16cffa415f20dff3ac47d7c1acbea404afc1a0fb
cecg7 <171274893+cecg7@users.noreply.github.com>
2025-01-23 11:57:54 -0600

    Labels_EEGfiles.m

--- Labels_EEGfiles.m
+++ Labels_EEGfiles.m
@@ -1,71 +1,29 @@
 %% WorkPaths
-% Saving data
-final_path = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost';
+% final_path = 'Your saving path'; Uncomment this line and include your saving path
 addpath('C:\Program Files\MATLAB\R2020a\eeglab2024.2');
 eeglab()
-%% Para extracción de etiquetas en EEGs
-read_paths = {};
+
+read_paths = {};        % declaring a variable to save all paths where reading experiment EEG signals were stored
+                        % Customize this by your number of paths (I used 4) 
 % PRE Reading
-% read_pre_base_C = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Reading\PRE\Baseline\Control';
-% read_pre_base_E = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Reading\PRE\Baseline\Experimental';
-read_paths{1} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Reading\PRE\Activity\Control';
-read_paths{2} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Reading\PRE\Activity\Experimental';
-
+read_paths{1} = 'MyPath1';
+read_paths{2} = 'MyPath2';
 % POST Reading
-% read_post_base_C = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Reading\POST\Baseline\Control';
-% read_post_base_E = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Reading\POST\Baseline\Experimental';
-read_paths{3} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Reading\POST\Activity\Control';
-read_paths{4} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Reading\POST\Activity\Experimental';
+read_paths{3} = 'MyPath3';
+read_paths{4} = 'MyPath4';
 
-math_paths = {};
-
+math_paths = {};        % declaring a variable to save all paths where reading experiment EEG signals were stored
+                        % Customize this by your number of paths (I used 8) 
 % PRE Math
-% math_pre_base_C = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Math\PRE\Baseline\Control';
-% math_pre_base_E = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Math\PRE\Baseline\Experimental';
-math_paths{1} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Math\PRE\Activity_B1\Control';
-math_paths{2} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Math\PRE\Activity_B1\Experimental';
-math_paths{3} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Math\PRE\Activity_B2\Control';
-math_paths{4} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Math\PRE\Activity_B2\Experimental';
-
+math_paths{1} = 'MyPath1';;
+math_paths{2} = 'MyPath2';
+math_paths{3} = 'MyPath3';
+math_paths{4} = 'MyPath4';
 % POST Math
-% math_post_base_C = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Math\POST\Baseline\Control';
-% math_post_base_E = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Math\POST\Baseline\Experimental';
-math_paths{5} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Math\POST\Activity_B1\Control';
-math_paths{6} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Math\POST\Activity_B1\Experimental';
-math_paths{7} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Math\POST\Activity_B2\Control';
-math_paths{8} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\EEG_RawData_PrePost\Math\POST\Activity_B2\Experimental';
-
-%% Para renombrar archivos EEG
-read_paths = {};
-% PRE Reading
-read_paths{1} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Reading\PRE\Baseline\Control';
-read_paths{2} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Reading\PRE\Baseline\Experimental';
-read_paths{3} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Reading\PRE\Activity\Control';
-read_paths{4} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Reading\PRE\Activity\Experimental';
-
-% POST Reading
-read_paths{5} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Reading\POST\Baseline\Control';
-read_paths{6} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Reading\POST\Baseline\Experimental';
-read_paths{7} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Reading\POST\Activity\Control';
-read_paths{8} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Reading\POST\Activity\Experimental';
-
-math_paths = {};
-
-% PRE Math
-math_paths{1} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Math\PRE\Baseline\Control';
-math_paths{2} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Math\PRE\Baseline\Experimental';
-math_paths{3} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Math\PRE\Activity_B1\Control';
-math_paths{4} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Math\PRE\Activity_B1\Experimental';
-math_paths{5} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Math\PRE\Activity_B2\Control';
-math_paths{6} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Math\PRE\Activity_B2\Experimental';
-
-% POST Math
-math_paths{7} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Math\POST\Baseline\Control';
-math_paths{8} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Math\POST\Baseline\Experimental';
-math_paths{9} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Math\POST\Activity_B1\Control';
-math_paths{10} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Math\POST\Activity_B1\Experimental';
-math_paths{11} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Math\POST\Activity_B2\Control';
-math_paths{12} = 'C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\Math\POST\Activity_B2\Experimental';
+math_paths{5} = 'MyPath5';
+math_paths{6} = 'MyPath6';
+math_paths{7} = 'MyPath7';
+math_paths{8} = 'MyPath8';
 
 %% Extracting Labels for Readings
 lenRead = length(read_paths);
@@ -76,7 +34,7 @@ for i = 1:lenRead
     set_files = dir([read_paths{i} '\*.set']);
     for n = 1:length({set_files.name})
             filename = set_files(n).name;
-            EEG = pop_loadset([set_files(n).folder '\' set_files(n).name]);  % Loading j-th subject activity dataset
+            EEG = pop_loadset([set_files(n).folder '\' set_files(n).name]);  % Loading n-th subject dataset
             read_t = {EEG.event.type}';
             subj = repmat([extractBefore(filename,'_R')],...
                 length({EEG.event.type}),1);
@@ -94,7 +52,7 @@ for i = 1:lenMath
     set_files = dir([math_paths{i} '\*.set']);
     for n = 1:length({set_files.name})
             filename = set_files(n).name;
-            EEG = pop_loadset([set_files(n).folder '\' set_files(n).name]);  % Loading j-th subject activity dataset
+            EEG = pop_loadset([set_files(n).folder '\' set_files(n).name]);  % Loading n-th subject dataset
             math_t = {EEG.event.type}';
             subj = repmat([extractBefore(filename,'_M')],...
                 length({EEG.event.type}),1);
@@ -103,14 +61,47 @@ for i = 1:lenMath
 end
 writetable(math_tags,fullfile(final_path,'Math_Tags.xlsx'));
 
+
+%% RENAME EEG FILES 
+% Working Paths
+read_paths = {};             % Customize this section as you please
+% PRE Reading
+read_paths{1} = 'MyPath1';
+read_paths{2} = 'MyPath2';
+read_paths{3} = 'MyPath3';
+read_paths{4} = 'MyPath4';
+
+% POST Reading
+read_paths{5} = 'MyPath5';
+read_paths{6} = 'MyPath6';
+read_paths{7} = 'MyPath7';
+read_paths{8} = 'MyPath8';
+
+math_paths = {};            % Customize this section as you please
+% PRE Math
+math_paths{1} = 'MyPath1';
+math_paths{2} = 'MyPath2';
+math_paths{3} = 'MyPath3';
+math_paths{4} = 'MyPath4';
+math_paths{5} = 'MyPath5';
+math_paths{6} = 'MyPath6';
+
+% POST Math
+math_paths{7} = 'MyPath7';
+math_paths{8} = 'MyPath8';
+math_paths{9} = 'MyPath9';
+math_paths{10} = 'MyPath10';
+math_paths{11} = 'MyPath11';
+math_paths{12} = 'MyPath12';
+
 %% Rename EEG files - Reading
 lenRead = length(read_paths);
-subject_data = readtable("C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\NewNames_Dataset.xlsx");
+subject_data = readtable("My table path"); % A relationship with older and newer names must be uploaded
 for i = 1:lenRead
     set_files = dir([read_paths{i} '\*.set']);
     for n = 1:length({set_files.name})
             filename = set_files(n).name;
-            EEG = pop_loadset([set_files(n).folder '\' set_files(n).name]);  % Loading j-th subject activity dataset
+            EEG = pop_loadset([set_files(n).folder '\' set_files(n).name]);  % Loading n-th subject activity dataset
             new_name = extractBefore(filename,'_');
             new_name = table({new_name});
             new_name.Properties.VariableNames("Var1") = "SUBJECT";
@@ -125,12 +116,12 @@ end
 
 %% Rename EEG files - Math
 lenMath = length(math_paths);
-subject_data = readtable("C:\Users\52477\Desktop\Doctorado\8vo Semestre\ResultadosFinales_EEG\Raw EEG Data and Data Note\NewNames_Dataset.xlsx");
+subject_data = readtable("My table path"); % A relationship with older and newer names must be uploaded
 for i = 1:lenMath
     set_files = dir([math_paths{i} '\*.set']);
     for n = 1:length({set_files.name})
             filename = set_files(n).name;
-            EEG = pop_loadset([set_files(n).folder '\' set_files(n).name]);  % Loading j-th subject activity dataset
+            EEG = pop_loadset([set_files(n).folder '\' set_files(n).name]);  % Loading n-th subject activity dataset
             new_name = extractBefore(filename,'_');
             new_name = table({new_name});
             new_name.Properties.VariableNames("Var1") = "SUBJECT";
